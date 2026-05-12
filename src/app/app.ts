6fffa28e5451e1189f288e7f9795fc39586e00e5
kija mnada <tanprograma@gmail.com>
2026-05-12 15:30:28 +0300

    new static

--- src/app/app.ts
+++ src/app/app.ts
@@ -5,8 +5,8 @@ import { RouterOutlet } from '@angular/router';
   selector: 'app-root',
   imports: [RouterOutlet],
   templateUrl: './app.html',
-  styleUrl: './app.scss'
+  styleUrl: './app.scss',
 })
 export class App {
-  protected readonly title = signal('aziz');
+  protected readonly title = signal('amdiagnostics');
 }
