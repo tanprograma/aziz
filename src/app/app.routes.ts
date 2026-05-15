8369453196b57d8de4f43b5fea696e47da2808e3
kija mnada <tanprograma@gmail.com>
2026-05-15 09:38:22 +0300

    fixed routing

--- src/app/app.routes.ts
+++ src/app/app.routes.ts
@@ -2,7 +2,7 @@ import { Routes } from '@angular/router';
 import { Home } from './pages/home/home';
 
 export const routes: Routes = [
-     { path: '', pathMatch: 'full', redirectTo: '/home' },
+     { path: '', component: Home },
   {
     path: 'home',
     component: Home,
