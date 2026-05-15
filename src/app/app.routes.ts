fea862fcaa40c8317a30b0b97c8d14f4b8dda297
kija mnada <tanprograma@gmail.com>
2026-05-15 09:23:55 +0300

    without ssr

--- src/app/app.routes.ts
+++ src/app/app.routes.ts
@@ -1,36 +1,10 @@
 import { Routes } from '@angular/router';
 import { Home } from './pages/home/home';
 
-import { RenderMode } from '@angular/ssr';
-
 export const routes: Routes = [
-  { path: '', pathMatch: 'full', redirectTo: '/home' },
+     { path: '', pathMatch: 'full', redirectTo: '/home' },
   {
     path: 'home',
     component: Home,
-  },
-
-  //   {
-  //     path: 'view-tracer/:tracerID',
-  //     component: TracerManageComponent,
-  //   },
-  //   {
-  //     path: 'statistics',
-  //     component: StatisticsComponent,
-  //     loadChildren: () =>
-  //       import('./routes/statistics.routes').then((mod) => mod.routes),
-  //   },
-  //   { path: 'login', component: LoginComponent },
-  //   {
-  //     path: 'admin',
-  //     component: AdminComponent,
-  //     loadChildren: () =>
-  //       import('./routes/admin.routes').then((mod) => mod.routes),
-  //   },
-  //   {
-  //     path: 'shop/:name/:id',
-  //     component: ShopComponent,
-  //     loadChildren: () =>
-  //       import('./routes/shop.routes').then((mod) => mod.routes),
-  //   },
+  }
 ];
